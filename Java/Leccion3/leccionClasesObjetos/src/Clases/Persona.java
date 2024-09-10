01465d2e1bcd5a8c4c3d646835b9abfa52aa5da9
Carlos Ariel Diaz Cattoni <charlydiazcattoni@gmail.com>
2024-09-09 22:26:00 -0300

    Java - Clase 04

--- Java/Leccion3/leccionClasesObjetos/src/Clases/Persona.java
+++ Java/Leccion3/leccionClasesObjetos/src/Clases/Persona.java
@@ -5,4 +5,11 @@ public class Persona {
     // Atributos de la clase (características)
     String nombre;
     String apellido;
+    
+    // Métodos de la clase (Acciones)
+    public void obtenerInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
 }
+
---
+++ Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -0,0 +1,14 @@
+
+package Operaciones;
+
+public class Aritmetica {
+    // Atributos de la clse
+    int a;
+    int b;
+    
+    // Metodo
+    public void sumarNumeros(){
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+}
