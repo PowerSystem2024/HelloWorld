238f6d4850f36577194c364bf0f44c2f0f4824a9
Danna Martinez <dannamartinez1995@gmail.com>
2024-09-14 19:38:01 -0300

    clase 5 portafolio parte 2
    correcciones, y terminaciones.

--- Portafolio/Portafolio-vueJs/src/components/QuienesSomos.vue
+++ Portafolio/Portafolio-vueJs/src/components/QuienesSomos.vue
@@ -1,11 +1,15 @@
 <script setup>
 import { ref } from 'vue';
-
+const titulo = 'Hello World';
+const descripcion = 'Grupo de estudiantes de la Tecnicatura Universitaria en Programación - UTN';
+const presentacion = 'Hola, Bienvenido a nuestro portafolio de proyectos. Somos 5 estudiantes de la TUP aprendiendo sobre desarollo de aplicaciones web y móviles';
+const residencia = 'Argentina';
+const telefono = '+54 9 261-5522560';
 const redsocial = ref([
-    {id:1, nombre:'Linkedin', src:'/src/assets/linkedin_icon.svg', url:""},
-    {id:2, nombre:'Instagram', src:'/src/assets/instagram_icon.svg', url:""},
-    {id:3, nombre:'Facebook', src:'/src/assets/github_icon.svg', url:""},
-    {id:4, nombre:'Curriculum Vitae',src:'/src/assets/cv_resume_icon.svg', url:""}
+    {id:1, nombre:'Linkedin', src:'/src/components/icons/linkedin_icon.svg', url:""},
+    {id:2, nombre:'Instagram', src:'/src/components/icons/instagram_icon.svg', url:""},
+    {id:3, nombre:'Facebook', src:'/src/components/icons/github_icon.svg', url:""},
+    {id:4, nombre:'Curriculum Vitae',src:'/src/components/icons/cv_resume_icon.svg', url:""}
 
 ])
 </script>
@@ -13,26 +17,46 @@ const redsocial = ref([
 <template>
 <section class="quienes-somos">
     <div class="card">
-        <h1>Hello World</h1>
-        <h2> Grupo de estudiantes de la Tecnicauta Universitaria en Programación - UTN</h2>
-        <p> Hola, Bienvenido a nuestro portafolio de proyectos. Somos 5 estudiantes de la TUP aprendiendo sobre desarollo de aplicaciones web y móviles</p>
+        <h1>{{titulo}}</h1>
+        <h2> {{ descripcion }}</h2>
+        <p> {{ presentacion }}</p>
         <ul class="container-lista">
-            <a v-for="nav in redsocial" :key="nav.nombre" href="nav.enlace" class="icon-redsocial"> {{ nav.nombre }}</a>
+           <li v-for="red in redsocial" :key="red.id">
+                <a href="red.url"><img class="icon-redsocial" :src="red.src" width="35rem" :alt="red.name"></a>
+           </li>
         </ul>
-        <h3>Nuestro telefono es: +54 9 261-5522560</h3>
-        <h4> Argentina </h4>
+        <h3>Nuestro telefono es: {{ telefono }}</h3>
+        <h4> {{ residencia }} </h4>
     </div>
 </section>
 </template>
 
 <style scoped>
+h1{
+    font-size: 2.5rem;
+}
+
+p{
+    font-size: 1.2rem;
+    font-weight: 600;
+    margin-bottom: 1rem;
+}
+
+.card {
+    background-color: rgb(28, 41, 52);
+    border-radius: 10px;
+    padding: 10px;
+    margin: 10px;
+    text-align: center;
+}
+
 .container-lista{
     display: flex;
-    flex-wrap: wrap;
-    gap: 10px;
     justify-content: center;
-    padding: 10px;
-    background-color: rgba(92, 129, 231, 0.934);
+   list-style: none;
+   padding: 0;
+   margin: 0;
+   text-align: center;
 }
 
 .icon-redsocial{
