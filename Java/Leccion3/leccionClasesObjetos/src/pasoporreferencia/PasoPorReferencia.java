137b20fcffd087f33fd00fa1d4bb181b5af80c83
Carlos Ariel Diaz Cattoni <charlydiazcattoni@gmail.com>
2024-09-22 22:27:13 -0300

    Java - Clase 06 - Parte 4 - PasoPorReferencia2

--- Java/Leccion3/leccionClasesObjetos/src/pasoporreferencia/PasoPorReferencia.java
+++ Java/Leccion3/leccionClasesObjetos/src/pasoporreferencia/PasoPorReferencia.java
@@ -12,11 +12,26 @@ public class PasoPorReferencia {
         System.out.println("persona1 nombre = " + persona1.nombre);
         cambioValor(persona1);
         System.out.println("El cambio que hicimos en el nombre es: "+persona1.nombre);
-       
+        persona1 = cambiarElValor(persona1);
+        Persona persona2 = new Persona();
+        //Persona persona2 = null;
+        persona2 = cambiarElValor(persona2);
+        System.out.println("El nuevo valor del objeto es: " + persona2.nombre);
     }
     
     public static void cambioValor(Persona persona){ // Paso por referencia
         persona.nombre = "María";
+    }
+    
+    public static Persona cambiarElValor(Persona persona){
+        if(persona == null){
+            System.out.println("El valor de persona es inválido: Null");
+            return null;
+        }
+        else{
+            persona.nombre = "Mónica";
+            return persona;
+        }        
     }
     
 }
